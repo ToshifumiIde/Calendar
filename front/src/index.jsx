aee3d4c152fefd6c1b4cc7f2d77ab3ab9f2c8b75
ToshifumiIde <toshifumiide198765@gmail.com>
2020-11-08 17:17:32 +0900

    feature0の作成（カレンダーの日付配列作成、表示に反映）

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CalendarBoard from "./components/CalendarBoard";//カレンダー全体の描画
 
+//カレンダーの日付を取得する配列を作成し、カレンダーに反映させる
+//feature0
+
 const App = () => (
 <div>
   <CalendarBoard />
